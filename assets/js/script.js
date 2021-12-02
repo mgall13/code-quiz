4fd1ff99c407ec98cc8c730870befd9e2cb3131c
Kyro <mariogallardo1331@yahoo.com>
2021-12-01 22:03:00 -0600

    adding questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 // Declare global variables
-var time = document.getElementById("time");
+var currentQuestionIndex = 0;
+var timeEl = document.getElementById("time");
+
 var startScreen = document.querySelector("#start-screen");
 var startBtn = document.getElementById("start-btn")
+var submitBtn = document.getElementById("submit-btn")
 var questionScreen = document.getElementById("question-screen");
-var questionIndex = 0;
+var endScreen = document.getElementById("end-screen")
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+var choicesEl = document.getElementById("choices");
+var showScore = document.getElementById("final-score")
 
-console.log("HEllo")
 
 var questions = [
+    {
+        questionH2: "Commonly used data types DO NOT include:",
+        choices: ["1", "2", "3", "4"],
+        answer: "3"
+    },
     {
         questionH2: "Enter question here",
         choices: ["1", "2", "3", "4"],
@@ -17,24 +30,42 @@ var questions = [
         questionH2: "Enter question here",
         choices: ["1", "2", "3", "4"],
         answer: "3"
-    }
-]
+    },
+    {
+        questionH2: "A very useful tool used during development and debugging for printing content to the bugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "quotes"
+}
+];
 
-
+var finalQuestionIndex = questions.length;
 
 // Start Function
 function startQuiz() {
+    // hiding our start screen
     startScreen.setAttribute("class", "hide");
+    // un-hiding our question screen
     questionScreen.removeAttribute("class", "hide");
-
-
-
-
+    getQuestions();
 }
 
-
 // Function Get questions
+function getQuestions() {
 
+    if (currentQuestionIndex === finalQuestionIndex) {
+        return showScore();
+    }
+
+    // get current question from array
+    var currentQuestion = questions[currentQuestionIndex];
+
+    // creating paragraph element to hold our question
+    questionScreen.innerHTML = "<p>" + currentQuestion.questionH2 + "</p>";
+    currentQuestion.questionH2 = currentQuestion
+
+};
+
+var finalQuestionIndex = questions.length;
 
 
 // Function to handle choosing a answer
